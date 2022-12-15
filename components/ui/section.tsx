81e9ef767ef6954579d1eb45fd9a2409b9567203
Lukasz <luke.filipiuk@gmail.com>
2022-12-15 13:16:30 +0100

    Fixing paddings and margins, added components

--- components/ui/section.tsx
+++ components/ui/section.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import styles from "./Section.module.css";
 
 type Props = {
-  children: JSX.Element;
+  children?: JSX.Element | JSX.Element[];
 };
 
 const Section = ({ children }: Props) => {
-  return <section className={"max-w-2xl mx-auto my-11"}>{children}</section>;
+  return <section className={"max-w-2xl mx-auto my-14"}>{children}</section>;
 };
 
 export default Section;
--- pages/index.tsx
+++ pages/index.tsx
@@ -36,203 +36,187 @@ export default function Home() {
 
       <main className={"bg-gray-900 font-inter my-32"}>
         <Section>
-          <>
-            <header className={"flex items-center justify-between"}>
-              <div className={"flex"}>
-                <div
-                  className={
-                    "bg-gradient-to-b from-teal-500 rounded-full w-16 h-16 relative overflow-hidden"
-                  }
-                >
-                  <Image
-                    src={lukasz}
-                    alt={"Picture of me"}
-                    width={200}
-                    height={200}
-                  />
+          <header className={"flex items-center justify-between"}>
+            <div className={"flex"}>
+              <div
+                className={
+                  "bg-gradient-to-b from-teal-500 rounded-full w-16 h-16 relative overflow-hidden"
+                }
+              >
+                <Image
+                  src={lukasz}
+                  alt={"Picture of me"}
+                  width={200}
+                  height={200}
+                />
+              </div>
+              <div className={"px-4"}>
+                <p className={"text-white font-newsreader text-2xl"}>
+                  Łukasz Filipiuk
+                </p>
+                <p className={"text-xl text-gray-500"}>Some text goes here</p>
+              </div>
+            </div>
+            <div className={"text-gray-600 hover:text-gray-200 text-4xl"}>
+              <a href={"https://www.linkedin.com/in/lfilipiuk/"}>
+                <AiFillLinkedin />
+              </a>
+            </div>
+          </header>
+        </Section>
+
+        <Section>
+          <Title name={"about me"} />
+          <p className={"leading-7 text-gray-100"}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            laboris.
+          </p>
+        </Section>
+
+        <Section>
+          <Title name={"i taught at"} />
+
+          <div className={"flex flex-wrap gap-2 items-center"}>
+            <div className={"flex-auto"}>
+              <Image src={euronet} alt={"Euronet Logo"} />
+            </div>
+            <div className={"flex-auto"}>
+              <Image src={bain} alt={"Bain & Company Logo"} />
+            </div>
+            <div className={"flex-auto"}>
+              <Image src={mars} alt={"Mars Logo"} />
+            </div>
+            <div className={"flex-auto"}>
+              <Image src={novo} alt={"Novo Nordisk Logo"} />
+            </div>
+            <div className={"flex-auto"}>
+              <Image src={mf} alt={"Ministry of Finance Logo"} />
+            </div>
+            <div className={"flex-auto"}>
+              <Image src={deloitte} alt={"Deloitte Logo"} />
+            </div>
+          </div>
+        </Section>
+
+        <Section>
+          <Title name={"portfolio"} />
+
+          <div
+            className={
+              "transition ease-in-out hover:-translate-y-1 hover:shadow-xl duration-200 cursor-pointer pt-2"
+            }
+            onClick={handlePortfolioClick}
+          >
+            <Image
+              className={"rounded-lg object-cover"}
+              width={700}
+              height={200}
+              src={portfolio}
+              alt={"Portfolio image"}
+            />
+          </div>
+        </Section>
+
+        <Section>
+          <Title name={"my experience"} />
+          <div className={"px-2 pt-4"}>
+            <ol>
+              <li className={"border-l-2 border-gray-600"}>
+                <div className="flex flex-start items-center">
+                  <div className="bg-gray-600 w-4 h-4 flex items-center justify-center rounded-full -ml-2 mr-3 -mt-2"></div>
+                  <h4 className="text-gray-200 font-semibold text-lg -mt-2 ml-1">
+                    Consulting at LabMasters and Exploring fullstack development
+                  </h4>
                 </div>
-                <div className={"px-4"}>
-                  <p className={"text-white font-newsreader text-2xl"}>
-                    Łukasz Filipiuk
+                <div className="ml-6 mb-6 pb-6">
+                  <p className="text-gray-400 text-gray-600-700 text-sm">Now</p>
+                  <p className="text-gray-400 mt-2 mb-4">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
                   </p>
-                  <p className={"text-xl text-gray-500"}>Some text goes here</p>
                 </div>
-              </div>
-              <div className={"text-gray-600 hover:text-gray-200 text-4xl"}>
-                <a href={"https://www.linkedin.com/in/lfilipiuk/"}>
-                  <AiFillLinkedin />
-                </a>
-              </div>
-            </header>
-          </>
+              </li>
+              <li className={"border-l-2 border-gray-600 -mt-6"}>
+                <div className="flex flex-start items-center">
+                  <div className="bg-gray-600 w-4 h-4 flex items-center justify-center rounded-full -ml-2 mr-3 -mt-2"></div>
+                  <h4 className="text-gray-200 font-semibold text-lg -mt-2 ml-1">
+                    Developer at Goldman Sachs & Teaching at the University of
+                    Warsaw
+                  </h4>
+                </div>
+                <div className="ml-6 mb-6 pb-6">
+                  <p className="text-gray-400 text-gray-600-700 text-sm">
+                    2000 -{">"} 2010
+                  </p>
+                  <p className="text-gray-400 mt-2 mb-4">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+                </div>
+              </li>
+
+              <li className={"-mt-6"}>
+                <div className="flex flex-start items-center">
+                  <div className="bg-gray-600 w-4 h-4 flex items-center justify-center rounded-full -ml-2 mr-3 -mt-2"></div>
+                  <h4 className="text-gray-200 font-semibold text-lg -mt-2 ml-1">
+                    Something
+                  </h4>
+                </div>
+                <div className="ml-6">
+                  <p className="text-gray-400 text-gray-600-700 text-sm">
+                    1990 -{">"} 2000
+                  </p>
+                  <p className="text-gray-400 mt-2">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+                </div>
+              </li>
+            </ol>
+          </div>
         </Section>
 
         <Section>
-          <>
-            <Title name={"about me"} />
-            <p className={"leading-7 text-gray-100"}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip Ut enim ad minim veniam, quis nostrud exercitation
-              ullamco laboris.
+          <Title name={"what others say about me"} />
+          <div className={"text-white italic text-gray-400 py-2 leading-7"}>
+            <p>
+              “I am once again very impressed with the{" "}
+              <span className={"font-bold text-gray-200"}>
+                knowledge and preparation
+              </span>{" "}
+              as well as the PASSION with which the Leaders convey this
+              knowledge to the Participants“
             </p>
-          </>
-        </Section>
-
-        <Section>
-          <>
-            <Title name={"i taught at"} />
-
-            <div className={"flex flex-wrap gap-2 items-center"}>
-              <div className={"flex-auto"}>
-                <Image src={bain} alt={"Bain & Company Logo"} />
-              </div>
-              <div className={"flex-auto"}>
-                <Image src={euronet} alt={"Euronet Logo"} />
-              </div>
-              <div className={"flex-auto"}>
-                <Image src={mars} alt={"Mars Logo"} />
-              </div>
-              <div className={"flex-auto"}>
-                <Image src={novo} alt={"Novo Nordisk Logo"} />
-              </div>
-              <div className={"flex-auto"}>
-                <Image src={mf} alt={"Ministry of Finance Logo"} />
-              </div>
-              <div className={"flex-auto"}>
-                <Image src={deloitte} alt={"Deloitte Logo"} />
-              </div>
-            </div>
-          </>
-        </Section>
-
-        <Section>
-          <>
-            <Title name={"portfolio"} />
-
-            <div
-              className={
-                "transition ease-in-out hover:-translate-y-1 hover:shadow-xl duration-200 cursor-pointer"
-              }
-              onClick={handlePortfolioClick}
-            >
-              <Image
-                className={"rounded-lg object-cover"}
-                width={700}
-                height={200}
-                src={portfolio}
-                alt={"Portfolio image"}
-              />
-            </div>
-          </>
-        </Section>
-
-        <Section>
-          <>
-            <Title name={"my experience"} />
-            <div className={"px-2"}>
-              <ol>
-                <li className={"border-l-2 border-gray-600"}>
-                  <div className="flex flex-start items-center">
-                    <div className="bg-gray-600 w-4 h-4 flex items-center justify-center rounded-full -ml-2 mr-3 -mt-2"></div>
-                    <h4 className="text-gray-200 font-semibold text-lg -mt-2 ml-1">
-                      Consulting at LabMasters and Exploring fullstack
-                      development
-                    </h4>
-                  </div>
-                  <div className="ml-6 mb-6 pb-6">
-                    <p className="text-gray-400 text-gray-600-700 text-sm">
-                      Now
-                    </p>
-                    <p className="text-gray-400 mt-2 mb-4">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </p>
-                  </div>
-                </li>
-                <li className={"border-l-2 border-gray-600 -mt-6"}>
-                  <div className="flex flex-start items-center">
-                    <div className="bg-gray-600 w-4 h-4 flex items-center justify-center rounded-full -ml-2 mr-3 -mt-2"></div>
-                    <h4 className="text-gray-200 font-semibold text-lg -mt-2 ml-1">
-                      Developer at Goldman Sachs & Teaching at the University of
-                      Warsaw
-                    </h4>
-                  </div>
-                  <div className="ml-6 mb-6 pb-6">
-                    <p className="text-gray-400 text-gray-600-700 text-sm">
-                      2000 -{">"} 2010
-                    </p>
-                    <p className="text-gray-400 mt-2 mb-4">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </p>
-                  </div>
-                </li>
-
-                <li className={"-mt-6"}>
-                  <div className="flex flex-start items-center">
-                    <div className="bg-gray-600 w-4 h-4 flex items-center justify-center rounded-full -ml-2 mr-3 -mt-2"></div>
-                    <h4 className="text-gray-200 font-semibold text-lg -mt-2 ml-1">
-                      Something
-                    </h4>
-                  </div>
-                  <div className="ml-6">
-                    <p className="text-gray-400 text-gray-600-700 text-sm">
-                      1990 -{">"} 2000
-                    </p>
-                    <p className="text-gray-400 mt-2">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </p>
-                  </div>
-                </li>
-              </ol>
-            </div>
-          </>
-        </Section>
-
-        <Section>
-          <>
-            <Title name={"what others say about me"} />
-
-            <div className={"text-white italic text-gray-400 py-3 leading-7"}>
-              <p>
-                “I am once again very impressed with the{" "}
-                <span className={"font-bold text-gray-200"}>
-                  knowledge and preparation
-                </span>{" "}
-                as well as the PASSION with which the Leaders convey this
-                knowledge to the Participants“
-              </p>
-            </div>
-            <div className={"text-white italic text-gray-400 py-3 leading-7"}>
-              <p>
-                “The teacher was very well-prepared,{" "}
-                <span className={"font-bold text-gray-200"}>
-                  presented the discussed tools in an accessible
-                </span>{" "}
-                way and helped course participants in the event of problems with
-                taking the &quot;step by step&quot; approach.“
-              </p>
-            </div>
-            <div className={"text-white italic text-gray-400 py-3 leading-7"}>
-              <p>
-                “Łukasz has good contact with the course participants, he
-                explains the material very well, he can answer any question and{" "}
-                <span className={"font-bold text-gray-200"}>
-                  keep the participants active
-                </span>{" "}
-                despite the long training day.“
-              </p>
-            </div>
-          </>
+          </div>
+          <div className={"text-white italic text-gray-400 py-3 leading-7"}>
+            <p>
+              “The teacher was very well-prepared,{" "}
+              <span className={"font-bold text-gray-200"}>
+                presented the discussed tools in an accessible
+              </span>{" "}
+              way and helped course participants in the event of problems with
+              taking the &quot;step by step&quot; approach.“
+            </p>
+          </div>
+          <div className={"text-white italic text-gray-400 py-3 leading-7"}>
+            <p>
+              “Łukasz has good contact with the course participants, he explains
+              the material very well, he can answer any question and{" "}
+              <span className={"font-bold text-gray-200"}>
+                keep the participants active
+              </span>{" "}
+              despite the long training day.“
+            </p>
+          </div>
         </Section>
       </main>
     </div>
